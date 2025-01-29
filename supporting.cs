fe5dd48155b6804b89d6e0a5bae4879adbbabc5b
Jeffery YaTES <jefferybyates@gmail.com>
2025-01-29 09:45:57 -0700

    Separation of classes into their respective class files.

--- supporting.cs
+++ supporting.cs
@@ -1,6 +1,35 @@
 namespace Mission4_Group1_4;
 
-public class supporting
+class Supporting
 {
-    
+    public static void updateBoard(char[,] board)
+    {
+        Console.Clear();
+        Console.WriteLine("Tic-Tac-Toe Board:");
+        Console.WriteLine($" {board[0, 0]} | {board[0, 1]} | {board[0, 2]} ");
+        Console.WriteLine("---|---|---");
+        Console.WriteLine($" {board[1, 0]} | {board[1, 1]} | {board[1, 2]} ");
+        Console.WriteLine("---|---|---");
+        Console.WriteLine($" {board[2, 0]} | {board[2, 1]} | {board[2, 2]} ");
+    }
+
+    public static string getWinner(char[,] board)
+    {
+        // Check rows and columns
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                return $"Player {board[i, 0]} wins!";
+            if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                return $"Player {board[0, i]} wins!";
+        }
+
+        // Check diagonals
+        if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            return $"Player {board[0, 0]} wins!";
+        if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            return $"Player {board[0, 2]} wins!";
+
+        return null; // No winner yet
+    }
 }
